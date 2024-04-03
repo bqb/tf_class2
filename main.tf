93a816868671a8f834abb827c8336ba06d644672
bqb <bbq@TIS-89N3TT3.ad.sfgov.org>
2024-04-02 19:04:09 -0700

    now with improved variables

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
-# test out some local variables
-locals {
-    prefix = "geomeoso"
-    region = "westus2"
-    tags = {
-        cost_center = "Pauls wallet"
-    }
+variable prefix {}
+
+variable region {
+    type = string
+    default = "westus2"
 }
+
+variable tags {
+    type = map
+}
+
 
 # Specifiy the provider and version
 terraform {
@@ -24,16 +27,16 @@ provider "azurerm" {
 
 # Create main resource group for geomeoso
 resource "azurerm_resource_group" "geomeoso_rg" {
-    name = "${local.prefix}_rg"
-    location = local.region
-    tags = local.tags
+    name = "${var.prefix}_rg"
+    location = var.region
+    tags = var.tags
 
 }
 
 # Create dev resource group for geomeoso
 resource "azurerm_resource_group" "geomeoso_dev_rg" {
-    name = "${local.prefix}_dev_rg"
-    location = local.region
-    tags = local.tags
+    name = "${var.prefix}_dev_rg"
+    location = var.region
+    tags = var.tags
 
 }
